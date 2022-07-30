fe7bcf0c04816af9a71ff7871fb632f5c8cc83d7
Pavel Bezrukov <darud4@yandex.ru>
2022-07-30 23:00:20 +0400

    feat: add not found check to controllers

--- notes/controllers/notes.js
+++ notes/controllers/notes.js
@@ -42,7 +42,7 @@ module.exports.getOneNote = (req, res, next) => {
   return Note.findByPk(id)
     .then((note) => {
       if (!note) throw new NotFound(ERRMSG_NOTE_NOT_FOUND);
-      else if (note.uid === +uid)
+      if (note.uid === +uid)
         return res.status(200).send(note);
       throw new NotAuthorized(ERRMSG_SELECT_NOTE_NOT_YOURS);
     })
@@ -54,6 +54,7 @@ module.exports.updateNote = (req, res, next) => {
   const { id } = req.params;
   return Note.findByPk(id)
     .then((note) => {
+      if (!note) throw new NotFound(ERRMSG_NOTE_NOT_FOUND);
       if (note.uid === +uid) {
         return Note.update(
           { title, text, isPinned },
@@ -74,6 +75,7 @@ module.exports.deleteNote = (req, res, next) => {
   const { uid } = req.body;
   return Note.findByPk(id)
     .then((note) => {
+      if (!note) throw new NotFound(ERRMSG_NOTE_NOT_FOUND);
       if (note.uid === +uid) {
         return Note.destroy(
           { where: { id } },
